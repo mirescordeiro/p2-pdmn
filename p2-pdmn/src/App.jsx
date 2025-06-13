72eba66cdd8f03114867d4c1a0cb5ff0daa34421
Tamires Cordeiro <mirescordeiro@gmail.com>
2025-06-13 04:10:18 -0300

    feat(p2): adiciona componente de lista

--- p2-pdmn/src/App.jsx
+++ p2-pdmn/src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react';
 import Busca from './components/Busca';
+import ListaPrevisoes from './components/ListaPrevisoes';
 
 const App = () => {
-  const [listaPrevisoes, setListaPrevisoes] = useState([]);
+  const [listaPrevisoes, setListaPrevisoes] = useState(['',[]]);
 
   function aoRealizarBusca(lista) {
     setListaPrevisoes(lista);
   }
 
   return (
-    <div>
-      <Busca enviarResultadoBusca={aoRealizarBusca}/>
-      <p>{listaPrevisoes[0]}</p>
+    <div className='flex justify-content-center'>
+      <div className='w-8'>
+        <div className='col-12'>
+          <Busca enviarResultadoBusca={aoRealizarBusca}/>
+        </div>
+
+        <div className='flex-column col-12'>
+          <h1>{listaPrevisoes[0]}</h1>
+          <div className='flex flex-row justify-content-between flex-wrap row-gap-1'>
+            <ListaPrevisoes previsoes={listaPrevisoes[1]}/>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
